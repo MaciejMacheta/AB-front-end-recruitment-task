5e44f1ed24c1b7b9d9b94cb8098e121344aefaa5
DESKTOP-8A5LMT7\Maciej <maciej.macheta.work@gmail.com>
2022-09-10 20:22:00 +0200

    JS file created
    JS file created,
    function to open and exit from taskbar
    
    CSS and HTML
    taskbar created

---
+++ dist/all.js
@@ -0,0 +1,11 @@
+function taskbar() 
+{
+    const alert = document.querySelector('.taskbar');
+    alert.style.display = 'block'; 
+}
+
+function exit() 
+{
+    const alert = document.querySelector('.taskbar');
+    alert.style.display = 'none'; 
+}
