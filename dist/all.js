e599c404f94acd6cb49a41ac7b5de0051d3ef428
DESKTOP-8A5LMT7\Maciej <maciej.macheta.work@gmail.com>
2022-09-11 22:12:59 +0200

    Style and simple JS
    fixes
    -elements
    -colors
    -blocks
    js
    -simple animation about taskbar and tasks-elements

--- dist/all.js
+++ dist/all.js
@@ -2,11 +2,17 @@ function taskbar()
 {
     const alert = document.querySelector('.taskbar');
     alert.style.display = 'block'; 
-}
+    const alert2 = document.querySelector('.tasks-elements');
+    alert2.style.color = '#0062B1';
+    return true;
+};
 
 function exit() 
 {
     const alert = document.querySelector('.taskbar');
     alert.style.display = 'none'; 
-}
+    const alert2 = document.querySelector('.tasks-elements');
+    alert2.style.color = 'black';
+    return true;
+};
 
